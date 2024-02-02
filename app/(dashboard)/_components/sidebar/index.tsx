1989da8ec29816fc62d4fbe487537b5c84c81ddd
MehmetZiya <mehmetziya82@gmail.com>
2024-02-02 14:31:04 +0100

    Add new sidebar item component and list component to display user memberships and create organization button with a hint tooltip

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,8 +1,10 @@
+import List from './list'
 import NewButton from './new-button'
 
 export default function Sidebar() {
   return (
     <aside className='flex flex-col fixed z-[1] left-0 bg-blue-950 h-full w-[60px] p-3 gap-y-4 text-white'>
+      <List />
       <NewButton />
     </aside>
   )
