eb681fb807cfd939f40189ecf618dd6d5e47fda1
MehmetZiya <mehmetziya82@gmail.com>
2024-02-14 15:23:26 +0100

    Refactor board page to use the `Room` component and add a `Loading` fallback

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
 export default function Info() {
   return (
     <div className='absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md'>
       info
     </div>
+  )
+}
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className='absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]' />
   )
 }
