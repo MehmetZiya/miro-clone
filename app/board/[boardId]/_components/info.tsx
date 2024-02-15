c3f626e0e1b3342981d5e4b03138c8c8aa54d1b3
MehmetZiya <mehmetziya82@gmail.com>
2024-02-15 16:22:27 +0100

    feat: add skeleton components for Info, Participants, and Toolbar to display loading state

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,13 +1,38 @@
-export default function Info() {
+import React from 'react'
+
+import Actions from '@/components/actions'
+import { Hint } from '@/components/hint'
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { cn } from '@/lib/utils'
+import { useRenameModal } from '@/store/use-rename-modal'
+import { useQuery } from 'convex/react'
+import { SlidersHorizontal } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface InfoProps {
+  boardId: string
+}
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600'],
+})
+const TabSeparator = () => {
+  return <div className='text-neutral-300 px-1.5'>|</div>
+}
+export default function Info({ boardId }: InfoProps) {
+  const { onOpen } = useRenameModal()
+  const data = useQuery(api.board.get, { id: boardId as Id<'boards'> })
+
+  if (!data) {
+    return <InfoSkeleton />
+  }
   return (
     <div className='absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md'>
       info
     </div>
-  )
-}
-
-Info.Skeleton = function InfoSkeleton() {
-  return (
-    <div className='absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 center shadow-md w-[300px]' />
   )
 }
--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,13 +1,15 @@
+'use client'
+
+import { useOthers, useSelf } from '@/liveblocks.config'
+import UserAvatar from './user-avatar'
+import { connectionIdToColor } from '@/lib/utils'
+
+const MAX_PARTICIPANTS = 4
+
 export default function Participants() {
   return (
     <div className='absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md'>
       Participants
     </div>
-  )
-}
-
-Participants.Skeleton = function ParticipantsSkeleton() {
-  return (
-    <div className='absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]' />
   )
 }
